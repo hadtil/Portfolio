226b5aef3c1bf34dcf4e0f41f52f66c21b6661cc
hadtil <53307053+hadtil@users.noreply.github.com>
2019-10-14 23:36:37 +0100

    Displaying Comic with descriptive text.

--- index.js
+++ index.js
@@ -17,98 +17,54 @@ application.get('/', function(request, response) {
 });
 
 application.post('/', function(request, response) {
-  // response.send("Hello");
-  // var x = request.body.crypto;
-  // var y = request.body.fiat;
-  // response.send(x+y);
-  // // response.send(request.body.crypto);
-  // // console.log(request.body.crypto);
-  // console.log(x + y);
 
-  var euro = "EUR";
-  var pound = "GBP";
-  var dollar = "USD";
-  var bitcoin = "BTC";
-  var etherium = "ETH";
-  var lite = "LTC";
-
-  var crypto = request.body.crypto;
-  var fiat = request.body.fiat;
-
-  // var BaseURL = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/';
-  var BaseURL = 'https://xkcd.com/';
-
-  // '614/info.0.json';
+  var comic = Number(request.body.ComicNumber);
 
   // 'https://xkcd.com/info.0.json'; // Current Comic
   // 'https://xkcd.com/614/info.0.json'; // Specific Comic
 
-  var FinalURL = BaseURL + crypto + fiat;
+  // Latest comic number as at 2019-10-14 is 2214
+  var lastComicAvailable = 2214;
 
-  var goal = Number(request.body.CurrencyUnits);
+  if (comic > lastComicAvailable) {
+    comic = lastComicAvailable;
+  } else if (comic == 0) {
+    comic = lastComicAvailable; // Bit of a fudge here!!
+  }
 
-    // var url = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD';
-  // var url = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/';
+  var BaseURL = 'https://xkcd.com/';
+  var EndURL = '/info.0.json';
 
-  // req('http://www.google.com', function (error, response, body) {
+  FinalURL = BaseURL + comic + EndURL;
+
   req(FinalURL, function(error, reqresponse, body) {
-    var data = JSON.parse(body);
-    var datex = data.display_timestamp;
-    // var price = data.high;
-    var price = data.last;
-    // console.log(data);
-    // console.log(price);
+     var data = JSON.parse(body);
+     var comicImg = data.img;
+     // var comicTitle = data.title;
+     var comicTitle = data.safe_title;
+     var comicAlt = data.alt;
+     var comicNum = data.num;
+     var comicYear = data.year;
 
-    // response.send(String(price));
+     console.log(comicImg);
+     console.log(comicTitle);
 
+ // response.send(String(price));
 
+  // ********** TO WRITE MULTIPLE RESPONSES ****************
+  response.write('<h1>' + comicTitle + ' </h1>');
+  response.write('<img src="' + comicImg + '" alt="' + comicAlt + '"><br>');
+  // response.write('<h5>' + comicAlt + '</h5>');
+  response.write('<h3>' + comicAlt);
+  response.write('  [from <span style="color:blue;">' + comicYear + ' </span> (#<span style="color:darkorange;">' + comicNum + '</span>)]</h3>');
+  response.send();
 
-    // ********** TO WRITE MULTIPLE RESPONSES ****************
-    response.write('<h1> The current date is ' + datex + ' </h1>');
-    response.write('<h3>The price of ' + crypto + "&nbsp;is " + price + fiat + '</h3>');
-    response.write('<h2><span style="color:blue;">' + goal + ' </span><span style="color:darkorange;">' + crypto + '</span> amounts to <span style="color:red;">' + price * goal +'</span><span style="color:darkorange;"> ' + fiat + '</span></h2>');
-    response.send();
-
-
-
-    // console.log('error:', error); // Print the error if one occurred
-    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    // console.log('body:', body); // Print the HTML for the Google homepage.
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  // console.log('body:', body); // Print the HTML for the Google homepage.
   });
+
 });
-
-
-
-//
-// application.post('/result', function(req, res){
-// //  res.send('Successfully Posted');
-//
-// // Form handling
-// //var a = req.body.inputNumber1;
-// //var b = req.body.inputNumber2;
-// //var result = Number(a) + Number(b);
-//
-// var a = Number(req.body.inputNumber1);
-// var b = Number(req.body.inputNumber2);
-// var result = a + b;
-//
-//   res.send('<h1>The result is : ' + result + '</h1>');
-// });
-
-
-// application.post('/welcome', function(request, response){
-//   var fname = request.body.firstName;
-//   var lname = request.body.lastName;
-//   response.send('<h1>Hello ' + fname + ' ' + lname + ', your super hero name is ' + heroes.random() + '. Please stay away from the super villain ' + villains.random() +'.</h1><br>' + '<a href="/">Return Home</a>');
-// });
-//
-// application.get('/heroes', function(req, res){
-//   res.send('<a href="/">Home</a><br><h1>' + heroes.random() + "</h1>");
-// });
-//
-// application.get('/villain', function(req, res){
-//   res.send('<a href="/">Home</a><br><h1 style="color:red;">' + villains.random() + "</h1>");
-// });
 
 application.listen(port, function() {
   console.log("Application is running fine and perfect on port number " + port);

c1ad75a2389d24a1f470577de17f4b15824337c0
Duncan <201901108@studentmail.itpt.co.uk>
2019-10-17 15:46:59 +0100

    Added array for todos entered with loop in ejs template to output array as list elements.

--- app.js
+++ app.js
@@ -4,31 +4,33 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-var todo = "";
+var todos = [];
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
 
   // Taken from: https://stackoverflow.com/questions/31378526/generate-random-date-between-two-dates-and-times-in-javascript/31379536#31379536
   var myRandomDateGenerator =
-         function(rangeOfDays, startHour, hourRange){
-              var today = new Date(Date.now());
-              return new Date(today.getYear() + 1900,
-                              today.getMonth(),
-                              today.getDate() +
-                                  Math.random() * rangeOfDays,
-                              Math.random() * hourRange +
-                                  startHour,
-                              Math.random() * 60);
-              //return new Date(2018, 11, 24, 10, 33, 30, 0);
-              // return "Hello There";
-  };
+    function(rangeOfDays, startHour, hourRange) {
+      var today = new Date(Date.now());
+      return new Date(today.getYear() + 1900,
+        today.getMonth(),
+        today.getDate() +
+        Math.random() * rangeOfDays,
+        Math.random() * hourRange +
+        startHour,
+        Math.random() * 60);
+      //return new Date(2018, 11, 24, 10, 33, 30, 0);
+      // return "Hello There";
+    };
 
-  var chosenDay = myRandomDateGenerator(30,8,2);
+  var chosenDay = myRandomDateGenerator(30, 8, 2);
   // res.write('<html>' + chosenDay.toString() + '<br>');
 
-  var today  = new Date();
+  var today = new Date();
 
   // chosenDay = today;
   // var chosenDay = new Date('August 19, 1975 23:15:30');
@@ -36,12 +38,13 @@ app.get("/", function(req, res){
   var day1 = chosenDay.getDay();
   // Sunday - Saturday : 0 - 6
   var days = ["Sunday",
-              "Monday",
-              "Tuesday",
-              "Wednesday",
-              "Thursday",
-              "Friday",
-              "Saturday"];
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
 
   var day = "";
 
@@ -66,14 +69,21 @@ app.get("/", function(req, res){
 
   // See https://stackoverflow.com/questions/3552461/how-to-format-a-javascript-date
   // var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  var options = { weekday: 'long',
-                  month: 'long',
-                  day: 'numeric',
-                  year: 'numeric'};
+  var options = {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric'
+  };
 
   var currentDay = chosenDay.toLocaleDateString("en-GB", options);
   // var currentDay = chosenDay.toLocaleDateString("ja-JP", options);
-  res.render("list", {kindOfDay:day, nameOfDay:days[day1], newOutput: currentDay, newListItem:todo});
+  res.render("list", {
+    kindOfDay: day,
+    nameOfDay: days[day1],
+    newOutput: currentDay,
+    newListItem: todos
+  });
 
   // console.log(today.toLocaleDateString("en-US")); // 9/17/2016
   // console.log(today.toLocaleDateString("en-US", options)); // Saturday, September 17, 2016
@@ -83,8 +93,8 @@ app.get("/", function(req, res){
 
 app.post('/', function(req, res) {
 
-  todo = req.body.ToDoItem;
-
+  var todo = req.body.ToDoItem;
+  todos.push(todo); // Push element onto todos array.
 
 
   // res.send("Hello");
@@ -95,6 +105,6 @@ app.post('/', function(req, res) {
 });
 
 var myPort = 1358;
-app.listen(myPort, function(){
+app.listen(myPort, function() {
   console.log('Server started on port ' + myPort);
 });

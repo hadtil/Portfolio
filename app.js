69c6d6b1e3a56158ccfe3b6369a87db227f35498
Duncan <201901108@studentmail.itpt.co.uk>
2019-10-31 14:37:02 +0000

    Progress on changing ToDo List to using Database.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 //jshint esversion:6
 
 const express = require("express");
+const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+// const date = require(__dirname + "/date.js");
 
 const app = express();
 
@@ -11,11 +12,63 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = [];
+var dbname = "todolistDatabase";
+mongoose.connect('mongodb://localhost/' + dbname,
+                 {useNewUrlParser: true,
+                  useUnifiedTopology: true});
+
+const itemsSchema = new mongoose.Schema({
+ name: String
+});
+// var Schema = mongoose.Schema;
+// var blogSchema = new Schema({
+//   title:  String,
+//   author: String,
+//   body:   String,
+//   comments: [{ body: String, date: Date }],
+//   date: { type: Date, default: Date.now },
+//   hidden: Boolean,
+//   meta: {
+//     votes: Number,
+//     favs:  Number
+//   }
+// });
+
+// const items = [];
+
+const item = mongoose.model('Item', itemsSchema);
+
+const item1 = new item({
+  name:"Welcome to your todoList!!"
+});
+
+const item2 = new item({
+  name:"Hit the + (plus) button to add something"
+});
+
+const item3 = new item({
+  name:"Hit the checkbox to delete the task"
+});
+
+const defaultItems = [item1, item2, item3];
+
+// item.insertMany(defaultItems, function(err){
+//   if (err){
+//     console.log(err);
+//   } else {
+//     console.log("Successfully Saved default values");
+//   }
+// });
 
 app.get("/", function(req, res) {
-const day = date.getDate();
-  res.render("list", {listTitle: day, newListItems: items});
+//const day = date.getDate();
+
+  item.find({}, function(err, foundItems){
+    // console.log(foundItems);
+    res.render("list", {listTitle: "Today", newListItems: foundItems});
+  });
+
+  // res.render("list", {listTitle: "Today", newListItems: defaultItems});
 
 });
 

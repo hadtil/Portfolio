d463a497ce674efa6ed44d8b814615ff00e284d6
Duncan Wilson <dunc@weedragon.net>
2021-01-12 11:21:53 +0000

    Population Health App -> Making progress with demo Clock app.

---
+++ Python/GUI/PopulationHealthApp/Explorations/Kivy/Modern Clock/README.md
@@ -0,0 +1,11 @@
+### Roboto Font
+See https://fonts.adobe.com/fonts/roboto
+
+"Google
+Google Fonts collaborates with type designers, foundries and the design community worldwide to create a directory of open source fonts. The fonts are free to use, making beautiful type accessible to anyone for any project."
+
+Open source details
+Roboto is available via an open source license. 
+Licensed for Personal & Commercial Use. 
+
+(as at January 2021)
--- Python/GUI/PopulationHealthApp/Explorations/Kivy/Modern Clock/main.py
+++ Python/GUI/PopulationHealthApp/Explorations/Kivy/Modern Clock/main.py
@@ -1,8 +1,33 @@
 from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+from kivy.uix.button import Button, ButtonBehavior
+from kivy.core.window import Window
+from kivy.core.text import LabelBase
+from kivy.uix.image import Image
+from kivy.utils import get_color_from_hex
+
+class ImageButton(ButtonBehavior, Image):
+    pass
+
+# This imports ClockScreen from main.kv file - inherits from Screen class
+class ClockScreen(Screen):
+    pass
+
+class StopwatchScreen(Screen):
+    pass
 
 # Base class inherits from main App class that we imported
 class MainApp(App): 
-    pass
+    
+    def go_forward(self):
+        screen_manager = self.root.ids['screen_manager']
+        screen_manager.transition= SlideTransition(direction="right")
+        screen_manager.current = 'stopwatchscreen'
 
 
-MainApp().run()
+if __name__ == "__main__":
+    Window.clearcolor = get_color_from_hex('#101216')
+    LabelBase.register(name='Roboto', fn_regular='Roboto-Thin.ttf', fn_bold='Roboto-Medium.ttf')
+    MainApp().run()
+
+
